d83a643ab150b51ebe0dec202fa3bbd22b173813
pahaz <pahaz.blinov@gmail.com>
2015-03-17 20:14:04 +0500

    v1

---
+++ utils.py
@@ -0,0 +1,34 @@
+from urllib.parse import parse_qs
+
+
+def parse_http_post_data(environ):
+    """
+    Parse a HTTP post data form WSGI `environ` argument.
+    """
+    try:
+        request_body_size = int(environ.get("CONTENT_LENGTH", 0))
+    except ValueError:
+        request_body_size = 0
+
+    request_body = environ["wsgi.input"].read(request_body_size)
+    body_query_dict = parse_qs(request_body)
+
+    return body_query_dict
+
+
+def take_one_or_None(dict_, key):
+    """
+    Take one value by key from dict or return None.
+        >>> d = {"foo":[1,2,3], "baz":7}
+        >>> take_one_or_None(d, "foo")
+        1
+        >>> take_one_or_None(d, "bar") is None
+        True
+        >>> take_one_or_None(d, "baz")
+        7
+    """
+    val = dict_.get(key)
+    if type(val) in (list, tuple) and len(val) > 0:
+        val = val[0]
+    return val
+
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,7 @@
+from urllib.parse import parse_qs
+from utils import parse_http_post_data, take_one_or_None
+
+
 data_messages = [
     b'Name: user<br>Message: hi!',
     b'Name: user<br>Message: hi!',
@@ -5,11 +9,26 @@ data_messages = [
 
 
 def application(environ, start_response):
+    REQUEST_METHOD = environ['REQUEST_METHOD']
+
     status = '200 OK'
     headers = [('Content-type', 'text/html; charset=utf-8')]
     with open('main.html', 'rb') as f:
         template_bytes = f.read()
+
+
+    if REQUEST_METHOD == 'POST':
+        post = parse_http_post_data(environ)
+        name = take_one_or_None(post, b'name')
+        message = take_one_or_None(post, b'message')
+        data_messages.append(b"Name: " + name + b"<br>Message: " + message)
+        start_response(status, headers)
+        return [b'<script>window.location.href = "/";</script>']
+    else:
+        post = []
+
     messages = b'<hr>'.join(data_messages)
     template_bytes = template_bytes.replace(b'{{messages}}', messages)
+
     start_response(status, headers)
-    return [template_bytes]
+    return [template_bytes, b'<hr>', repr(post).encode('utf-8')]
