282429611b78e562070abe95c48c22b31f6a4977
pahaz <pahaz.blinov@gmail.com>
2015-03-17 19:48:58 +0500

    Fix bug

--- wsgi.py
+++ wsgi.py
@@ -5,11 +5,11 @@ data_messages = [
 
 
 def application(environ, start_response):
-    status = '200 OK' # HTTP Status
-    headers = [('Content-type', 'text/html; charset=utf-8')] # HTTP Headers
+    status = '200 OK'
+    headers = [('Content-type', 'text/html; charset=utf-8')]
     with open('main.html', 'rb') as f:
         template_bytes = f.read()
     messages = b'<hr>'.join(data_messages)
-    template_bytes.replace(b'{{messages}}', messages)
+    template_bytes = template_bytes.replace(b'{{messages}}', messages)
     start_response(status, headers)
     return [template_bytes]
