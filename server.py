723eb7f58ebace6c7d895617e0754826cdd13da1
pahaz <pahaz.blinov@gmail.com>
2015-03-17 19:45:55 +0500

    Typo fix

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from wsgiref.simple_server import make_server
 import wsgi
 
 PORT = 8001
-print("GO: http://127.0.0.1:{0}/".format(PORT))
+
+print("Open: http://127.0.0.1:{0}/".format(PORT))
 httpd = make_server('', PORT, wsgi.application)
 httpd.serve_forever()
