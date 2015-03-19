ee571b7cf8e347e090465b7aa24901ba650e035f
pahaz <pahaz.blinov@gmail.com>
2015-03-20 01:42:23 +0500

    Fixes for homework

--- utils.py
+++ utils.py
@@ -1,37 +1,69 @@
+import cgi
 from urllib.parse import parse_qs
+from warnings import warn
+from wsgiref.headers import Headers
+from wsgiref.util import request_uri
 
 
 def parse_http_get_data(environ):
-    return parse_qs(environ.get("QUERY_STRING", ""))
+    qs = environ.get("QUERY_STRING", "")
+    if isinstance(qs, bytes):
+        qs = qs.decode('utf-8')
+    return parse_qs(qs)
 
 
-def parse_http_post_data(environ):
+def parse_http_x_www_form_urlencoded_post_data(environ):
     """
-    Parse a HTTP post data form WSGI `environ` argument.
+    Parse HTTP 'application/x-www-form-urlencoded' post data.
     """
     try:
         request_body_size = int(environ.get("CONTENT_LENGTH", 0))
     except ValueError:
         request_body_size = 0
 
-    request_body = environ["wsgi.input"].read(request_body_size)
-    body_query_dict = parse_qs(request_body)
+    CONTENT_TYPE, CONTENT_TYPE_KWARGS = parse_http_content_type(environ)
+    if CONTENT_TYPE != 'application/x-www-form-urlencoded':
+        warn(" * WARNING * Used parse_http_x_www_form_urlencoded_post_data "
+             "when CONTENT_TYPE != 'application/x-www-form-urlencoded'")
+        return {}
 
+    request_body_bytes = environ["wsgi.input"].read(request_body_size)
+    request_body_text = request_body_bytes.decode('utf-8')
+    body_query_dict = parse_qs(request_body_text)
     return body_query_dict
 
 
-def take_one_or_None(dict_, key):
+def parse_http_content_type(environ):
+    return cgi.parse_header(environ.get('CONTENT_TYPE', ''))
+
+
+def parse_http_headers(environ):
+    h = Headers([])
+    for k, v in environ.items():
+        if k.startswith('HTTP_'):
+            name = k[5:]
+            h.add_header(name, v)
+    return h
+
+
+def parse_http_uri(environ):
+    return request_uri(environ)
+
+
+def get_first_element(dict_, key, default=None):
     """
     Take one value by key from dict or return None.
         >>> d = {"foo":[1,2,3], "baz":7}
-        >>> take_one_or_None(d, "foo")
+        >>> get_first_element(d, "foo")
         1
-        >>> take_one_or_None(d, "bar") is None
+        >>> get_first_element(d, "bar") is None
         True
-        >>> take_one_or_None(d, "baz")
+        >>> get_first_element(d, "bar", "") == ""
+        True
+        >>> get_first_element(d, "baz")
         7
     """
-    val = dict_.get(key)
+    val = dict_.get(key, default)
     if type(val) in (list, tuple) and len(val) > 0:
         val = val[0]
     return val
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,12 @@
-from utils import parse_http_post_data, take_one_or_None, parse_http_get_data
+import wsgiref.validate
 
+from utils import parse_http_x_www_form_urlencoded_post_data, \
+    get_first_element, parse_http_get_data, parse_http_headers, \
+    parse_http_content_type, parse_http_uri
+
+DEBUG = True
+STATIC_URL = '/static/'
+STATIC_ROOT = 'data'
 
 data_messages = [
     b'Name: user<br>Message: hi!',
@@ -7,25 +14,49 @@ data_messages = [
 ]
 
 
+@wsgiref.validate.validator
 def application(environ, start_response):
+    # https://www.python.org/dev/peps/pep-3333/#environ-variables
     REQUEST_METHOD = environ['REQUEST_METHOD']
-    POST = parse_http_post_data(environ)
+    CONTENT_TYPE, CONTENT_TYPE_KWARGS = parse_http_content_type(environ)
+    SERVER_PROTOCOL = environ['SERVER_PROTOCOL']
+    HEADERS = parse_http_headers(environ)
+    URI_PATH = environ['PATH_INFO']
+    URI_QUERY = environ['QUERY_STRING']
+    URI = parse_http_uri(environ)
+    POST = parse_http_x_www_form_urlencoded_post_data(environ)
     GET = parse_http_get_data(environ)
-    # print(REQUEST_METHOD)
-    # print(GET)
-    # print(POST)
 
     status = '200 OK'
     headers = [('Content-type', 'text/html; charset=utf-8')]
+
+    if URI_PATH == '/favicon.ico':
+        status = '404 Not Found'
+        start_response(status, headers)
+        return [b'']
+
+    if URI_PATH.startswith(STATIC_URL):
+        print('STATIC FILE DETECTED!')
+
+    if DEBUG:
+        print("{REQUEST_METHOD} {URI_PATH}?{URI_QUERY} {SERVER_PROTOCOL}\n"
+              "CONTENT_TYPE: {CONTENT_TYPE}; {CONTENT_TYPE_KWARGS}\n"
+              "POST: {POST}\n"
+              "GET: {GET}\n"
+              ":HEADERS:\n{HEADERS}\n"
+              .format(**locals()))
+
     with open('main.html', 'rb') as f:
         template_bytes = f.read()
 
     if REQUEST_METHOD == 'POST':
         status = '303 See Other'
         headers.append(('Location', '/'))
-        name = take_one_or_None(POST, b'name')
-        message = take_one_or_None(POST, b'message')
-        data_messages.append(b"Name: " + name + b"<br>Message: " + message)
+        name = get_first_element(POST, 'name', '')
+        message = get_first_element(POST, 'message', '')
+        data_message_text = "Name: {0}<br>Message: {1}".format(name, message)
+        data_message_bytes = data_message_text.encode('utf-8')
+        data_messages.append(data_message_bytes)
         start_response(status, headers)
         return [b'']
 
