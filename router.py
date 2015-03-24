2b80d369905892c7ee179235f15a051ed200dc53
pahaz <pahaz.blinov@gmail.com>
2015-03-24 19:30:49 +0500

    ROUTER

---
+++ router.py
@@ -0,0 +1,22 @@
+from __future__ import unicode_literals, print_function, generators, division
+
+__author__ = 'pahaz'
+
+
+class Router:
+    def __init__(self):
+        self._routs = {}
+
+    def add_route(self, url, controller):
+        self._routs[url] = controller
+
+    def resolve(self, url):
+        callback = self._routs.get(url)
+        if callback:
+            return callback
+        return self.default_route
+
+    def default_route(self, get, post):
+        status = '404 Not Found'
+        body = b''
+        return status, body
